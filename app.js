18fddc6bca80adacd38871bb7bfb9a6ea8d2fe11
Jonathan Chan <chanjon@flip1.engr.oregonstate.edu>
2022-07-28 03:00:03 -0700

    Working Create operation for Genres

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-// Static Files
-app.use(express.static('public'));
+// // Static Files
+// app.use(express.static('public'));
 
 /*
     ROUTES
@@ -41,86 +41,38 @@ app.get('/Genres', function(req, res)
     })                                                      // an object where 'data' is equal to the 'rows' we
 });  
 
-// query1 = `INSERT INTO Genres (genre) VALUES (${data['input-genre-name']})`; // received back from the query 
-
-app.post('/add-genre-ajax', function(req, res) 
-{
+// CREATE operation for Genres
+app.post('/add-genre-form', function(req, res){
     // Capture the incoming data and parse it back to a JS object
     let data = req.body;
 
     // Capture NULL values
-    let genreID = parseInt(data.genre_id);
+    let genreID = parseInt(data['input-genre-id']);
     if (isNaN(genreID))
     {
         genreID = 'NULL'
     }
 
     // Create the query and run it on the database
-    query1 = `INSERT INTO Genres (genre_id, genre) VALUES (${genreID}, '${data.genre}');`;
-    // query1 = `INSERT INTO Genres (genre) VALUES ('${data.genre}');`;
+    query1 = `INSERT INTO Genres (genre_id, genre) VALUES (${genreID}, '${data['input-genre-name']}');`;
     db.pool.query(query1, function(error, rows, fields){
 
-        // Check to see if there was an error
+        // Check to see if ther was an error
         if (error) {
-
+            
             // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
             console.log(error)
+            // res.send(JSON.stringify(error));
             res.sendStatus(400);
         }
+         // If there was no error, we redirect back to our Genres route, which automatically runs the SELECT * FROM Genres and
+        // presents it on the screen
         else
         {
-            // If there was no error, perform a SELECT * on Genres
-            query2 = `SELECT * FROM Genres;`;
-            db.pool.query(query2, function(error, rows, fields){
-
-                // If there was an error on the second query, send a 400
-                if (error) {
-                    
-                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-                    console.log(error);
-                    res.sendStatus(400);
-                }
-                // If all went well, send the results of the query back.
-                else
-                {
-                    res.send(rows);
-                }
-            })
+            res.redirect('/Genres')
         }
     })
 });
-
-// app.post('/Genres', function(res, req) {
-//     // Capture the incoming data and parse it back to a JS object
-//     let data = req.body;
-
-//     // Capture NULL values  => how do verify for strings? the professor showed for numbers.
-//     // let genre = data['input-genre-name'];
-//     // let genre = parseInt(data['input-genre-name'])
-//     // if (isNaN(genre))
-//     // {
-//     //     genre = 'NULL'
-//     // }
-//     query1 = `INSERT INTO Genres (genre) VALUES (${data['input-genre-name']})`;
-//     // query1 = `INSERT INTO Genres (genre) VALUES ('${data['input-genre-name']}')`;
-//     db.pool.query(query1, function(error, rows, fields){
-
-//         // Check to see if there was an error
-//         if (error) {
-
-//             // Log the error to the terminal so that we know what went wrong, and send the visitor to an HTTP 400 indicating it was a bad request.
-
-//             console.log(error)
-//             res.sendStatus(400);
-//         }
-
-//         // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM genres and
-//         // Presents it on the screen
-//         else {
-//             res.redirect('/Genres');
-//         }
-//     })
-// });
 
 // READ operation for Concessions_has_Items
 app.get('/Concessions', function(req, res)
